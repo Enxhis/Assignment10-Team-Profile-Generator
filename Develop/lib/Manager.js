be63a1f8e582d34340d906a81e3f33561b272041
Enxhi Salaj <enxhi_salaj@yahoo.com>
2020-11-12 16:11:41 -0500

    Manager.js passed all tests

--- Develop/lib/Manager.js
+++ Develop/lib/Manager.js
@@ -1,1 +1,23 @@
-// TODO: Write code to define and export the Manager class. HINT: This class should inherit from Employee.
+// import class Employee
+const Employee = require("./Employee");
+
+// class Manager extends parent class Employee
+class Manager extends Employee{
+    // constructor
+    constructor(name, id, email, officeNumber){
+        super(name, id, email);
+        this.officeNumber = officeNumber;
+    }
+
+    // getOfficeNumber() method
+    getOfficeNumber(){
+        return this.officeNumber;
+    }
+
+    // getRole method overriden
+    getRole(){
+        return "Manager";
+    }
+}
+
+module.exports = Manager;
